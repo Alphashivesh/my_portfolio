260876569b7a6003a9aab5e91d79ff06ac3ff172
Shivesh Kumar <149619368+Alphashivesh@users.noreply.github.com>
2025-09-03 16:31:55 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,7 @@ const Navbar: React.FC = () => {
     { label: 'Home', href: '#home' },
     { label: 'About', href: '#about' },
     { label: 'Education', href: '#education' },
+    { label: 'Experience', href: '#experience'},
     { label: 'Certificate', href: '#certificate' },
     { label: 'Skills', href: '#skills' },
     { label: 'Projects', href: '#projects' },
